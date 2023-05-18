ebf95d585acdc4db382ed153c67f24e17d428ffd
marian51 <mariusz.jozwiak@int.pl>
2023-05-18 21:43:20 +0200

    Add basic content and styles for left logo

--- src/components/LeftMenu.vue
+++ src/components/LeftMenu.vue
@@ -1,6 +1,12 @@
 <template>
-	<div>
-		<div class="logo">Logo</div>
+	<div class="left-menu">
+		<div class="logo">
+				<span class="material-symbols-outlined">brightness_5</span>
+			
+			<div class="logo-name">
+				<p>Learning App</p>
+			</div>			
+		</div>
 		<div class="left-menu-options">
 			<ul>
 				<li>Option</li>
@@ -19,5 +25,29 @@
 </script>
 
 <style>
+.left-menu {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	/* align-items: center; */
+	/* padding: 3rem; */
+	padding-top: 5rem;
+}
 
+.logo {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content:center ;
+	gap: 1rem;
+}
+
+.logo span {
+	color: var(--text-color-blue);
+	font-size: 3rem;
+}
+
+.logo-name p {
+	font-size: 1.5rem;
+}
 </style>
