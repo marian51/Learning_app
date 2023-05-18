3271860404172524d71dafdf1b4cc349b6346e38
marian51 <mariusz.jozwiak@int.pl>
2023-05-18 21:16:20 +0200

    Add basic left menu structure

---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<div class="logo">Logo</div>
+		<div class="left-menu-options">
+			<ul>
+				<li>Option</li>
+				<li>Option</li>
+				<li>Option</li>
+				<li>Option</li>
+				<li>Option</li>
+			</ul>
+		</div>
+		<div class="faq">FAQ</div>
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
